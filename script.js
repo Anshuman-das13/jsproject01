cfed77df399c794825603b38a97295b9885176aa
Anshuman-das13 <138345959+Anshuman-das13@users.noreply.github.com>
2023-10-13 16:18:34 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const a=document.getElementById('heading2')
+function giveup()
+{
+a.style.display = 'block'
+}
+function give(){
+ a.style.display = 'none'
+}
+function give(){
+    if(a.style.display='none')
+    {
+        alert('alredy in reset mode')
+    }
+}
